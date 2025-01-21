143ff5028cef16525a6291d337cc47483fade350
Tapas Jyoti <tapasjyoti8327@gmail.com>
2025-01-21 19:40:22 +0530

    Created a express server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Namaste from the dashboard....");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello....");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the server side....");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000....");
+});
