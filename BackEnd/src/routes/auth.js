b79f13d190190d2f22d2ea1fafab37ed71aa68b5
Tapas Jyoti <tapasjyoti8327@gmail.com>
2025-03-23 23:59:25 +0530

    cookies working

--- BackEnd/src/routes/auth.js
+++ BackEnd/src/routes/auth.js
@@ -54,7 +54,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       });
-      res.send("Login successful");
+      res.send(user);
     } else {
       throw new Error("Invalid Credentials");
     }
--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+    } catch (error) {
+      console.error("Error in login", error);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-auto p-4 sm:p-6 lg:p-12">
+      <div className="card card-border bg-base-300 w-full max-w-md">
+        <div className="card-body">
+          <h2 className="card-title justify-center h-10 font-bold">Login</h2>
+          <div className="">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                value={emailId}
+                className="input w-full"
+                onChange={(e) => {
+                  setEmailId(e.target.value);
+                }}
+              />
+            </fieldset>
+            <fieldset className="fieldset mt-4">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                value={password}
+                className="input w-full"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-left">
+            <button
+              className="btn btn-primary mt-10 w-full"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
