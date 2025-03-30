2c1f573aa7c801ff5f65856a474a5e9593db67a0
Tapas Jyoti <tapasjyoti8327@gmail.com>
2025-03-30 23:24:24 +0530

    created cards for users

--- FrontEnd/src/components/Feed.jsx
+++ FrontEnd/src/components/Feed.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
-const LandingPage = () => {
+const FeedPage = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
   return (
-    <div>feed</div>
-  )
-}
+    feed && (
+      <div className="flex flex-col items-center justify-center p-4 sm:p-6 lg:p-12">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
+};
 
-export default LandingPage
+export default FeedPage;
---
+++ FrontEnd/src/components/UserCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const UserCard = ({ user }) => {
+  const { firstName, lastName, about, Skills, gender, age } = user;
+  return (
+    <div className="card bg-base-300 w-90 shadow-lg">
+      <figure>
+        <img src={user?.photoUrl} alt="PhotoUrl" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        <p>{about}</p>
+        <h3>{gender ? `Gender: ${gender}` : null}</h3>
+        <h3>{age ? `Age: ${age}` : null}</h3>
+        <h3>{Skills ? `Skills: ${Skills}` : null}</h3>
+        <div className="card-actions mt-10 space-x-10 justify-center">
+          <button className="cursor-pointer text-black hover:bg-amber-50 h-9 w-32 bg-red-500 badge">
+            Ignored
+          </button>
+          <button className="cursor-pointer text-black hover:bg-amber-50 h-9 w-32 bg-green-500 badge">
+            Connect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
